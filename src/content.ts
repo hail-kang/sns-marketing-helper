7e50c679ab810af4136b316bc2e0264e0f922bc7
hail-kang-tagby <hail1225@naver.com>
2024-10-22 16:05:35 +0900

    통계 모달 추가

--- src/content.ts
+++ src/content.ts
@@ -456,6 +456,10 @@ if (targetElement && targetElement.firstElementChild) {
     buttonElement.setAttribute("href", "#")
     buttonElement.addEventListener("click", () => {
       console.log("hello world")
+      const statisticModal = document.querySelector(
+        "#sns-helper-statistics",
+      ) as HTMLElement
+      statisticModal.style.display = "block"
     })
   }
 
@@ -486,4 +490,43 @@ if (targetElement && targetElement.firstElementChild) {
   }
 
   targetElement.prepend(firstElement)
+}
+
+const sectionContainer = document.querySelector(
+  "html > body > div:nth-of-type(2) > div > div > div > div:nth-of-type(2) > div > div > div:first-of-type > div:first-of-type > div:first-of-type",
+) as HTMLElement
+const sectionElement = sectionContainer?.querySelector("section")
+if (sectionContainer && sectionElement) {
+  const statisticModal = document.createElement("div")
+  statisticModal.id = "sns-helper-statistics"
+  statisticModal.textContent = "hello"
+  statisticModal.style.position = "fixed"
+  statisticModal.style.top = "0"
+  statisticModal.style.left = sectionContainer.style.marginLeft
+  statisticModal.style.background = "black"
+  statisticModal.style.width = `${sectionElement.offsetWidth}px`
+  statisticModal.style.height = "100%" // 높이를 100%로 설정
+  statisticModal.style.opacity = "0.7" // 불투명도 0.7로 설정
+  statisticModal.style.display = "none"
+  sectionContainer.appendChild(statisticModal)
+
+  const bodyBackgroundColor = window.getComputedStyle(
+    document.body,
+  ).backgroundColor
+  const isBlackBackground = bodyBackgroundColor === "rgb(0, 0, 0)"
+  const strokeColor = isBlackBackground ? "#FFFFFF" : "#000000"
+
+  const closeButton = document.createElement("div")
+  closeButton.innerHTML = `
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M13.06 12L17.48 7.57996C17.5537 7.5113 17.6128 7.4285 17.6538 7.3365C17.6948 7.2445 17.7168 7.14518 17.7186 7.04448C17.7204 6.94378 17.7018 6.84375 17.6641 6.75036C17.6264 6.65697 17.5703 6.57214 17.499 6.50092C17.4278 6.4297 17.343 6.37356 17.2496 6.33584C17.1562 6.29811 17.0562 6.27959 16.9555 6.28137C16.8548 6.28314 16.7555 6.30519 16.6635 6.34618C16.5715 6.38717 16.4887 6.44627 16.42 6.51996L12 10.94L7.58 6.51996C7.43782 6.38748 7.24978 6.31535 7.05548 6.31878C6.86118 6.32221 6.67579 6.40092 6.53838 6.53834C6.40096 6.67575 6.32225 6.86113 6.31882 7.05544C6.3154 7.24974 6.38752 7.43778 6.52 7.57996L10.94 12L6.52 16.42C6.37955 16.5606 6.30066 16.7512 6.30066 16.95C6.30066 17.1487 6.37955 17.3393 6.52 17.48C6.66062 17.6204 6.85125 17.6993 7.05 17.6993C7.24875 17.6993 7.43937 17.6204 7.58 17.48L12 13.06L16.42 17.48C16.5606 17.6204 16.7512 17.6993 16.95 17.6993C17.1488 17.6993 17.3394 17.6204 17.48 17.48C17.6204 17.3393 17.6993 17.1487 17.6993 16.95C17.6993 16.7512 17.6204 16.5606 17.48 16.42L13.06 12Z" fill="${strokeColor}"/>
+  </svg>`
+  closeButton.style.position = "absolute"
+  closeButton.style.top = "10px"
+  closeButton.style.right = "10px"
+  closeButton.style.cursor = "pointer"
+  closeButton.addEventListener("click", () => {
+    statisticModal.style.display = "none"
+  })
+  statisticModal.appendChild(closeButton)
 }
